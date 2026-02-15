ecbdd47d546b1d4171bfa36f0d3f4dfc30d8bcd8
MadhanRa <m.ramadhan@hotmail.co.id>
2026-02-15 21:15:17 +0700

    custom css file rename

--- app/Views/layout/backend.php
+++ app/Views/layout/backend.php
@@ -11,15 +11,17 @@
   <link rel="stylesheet" href="<?= base_url('template/node_modules/@fortawesome/fontawesome-free/css/all.css') ?>">
 
   <!-- CSS Libraries -->
-  <link rel="stylesheet" href="<?= base_url('template/node_modules/datatables.net-bs4/css/dataTables.bootstrap4.min.css') ?>">
-  <link rel="stylesheet" href="<?= base_url('template/node_modules/datatables.net-select-bs4/css/select.bootstrap4.min.css') ?>">
+  <link rel="stylesheet"
+    href="<?= base_url('template/node_modules/datatables.net-bs4/css/dataTables.bootstrap4.min.css') ?>">
+  <link rel="stylesheet"
+    href="<?= base_url('template/node_modules/datatables.net-select-bs4/css/select.bootstrap4.min.css') ?>">
   <!-- CSS Libraries -->
   <link rel="stylesheet" href="<?= base_url('template/node_modules/izitoast/dist/css/iziToast.min.css') ?>">
 
   <!-- Template CSS -->
   <link rel="stylesheet" href="<?= base_url('template/assets/css/style.css') ?>">
   <link rel="stylesheet" href="<?= base_url('template/assets/css/components.css') ?>">
-  <link rel="stylesheet" href="<?= base_url('template/assets/css/custom.css') ?>">
+  <link rel="stylesheet" href="<?= base_url('template/assets/css/custom.v2.css') ?>">
 
 
   <!-- JQuery -->
@@ -80,8 +82,10 @@
           </ul>
         </form>
         <ul class="navbar-nav navbar-right">
-          <li class="dropdown"><a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
-              <img alt="image" src="<?= base_url() ?>/template/assets/img/avatar/avatar-1.png" class="rounded-circle mr-1">
+          <li class="dropdown"><a href="#" data-toggle="dropdown"
+              class="nav-link dropdown-toggle nav-link-lg nav-link-user">
+              <img alt="image" src="<?= base_url() ?>/template/assets/img/avatar/avatar-1.png"
+                class="rounded-circle mr-1">
               <div class="d-sm-none d-lg-inline-block"><?= user()->username ?></div>
             </a>
             <div class="dropdown-menu dropdown-menu-right">
@@ -90,7 +94,7 @@
                 <?php } else { ?>
                   <a href="/login" class="dropdown-item has-icon text-danger"><i class="fas fa-sign-out-alt"></i> Login
                   <?php } ?>
-                  </a>
+                </a>
             </div>
           </li>
         </ul>
@@ -117,7 +121,8 @@
 
       <footer class="main-footer">
         <div class="footer-left">
-          Copyright &copy; 2024 <div class="bullet"></div> Design By <a href="https://eureekagreatnusantara.com/">Eureeka Great Nusantara</a>
+          Copyright &copy; 2024 <div class="bullet"></div> Design By <a
+            href="https://eureekagreatnusantara.com/">Eureeka Great Nusantara</a>
         </div>
         <div class="footer-right">
           2.3.0
@@ -128,7 +133,9 @@
 
   <!-- General JS Scripts -->
 
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+    integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+    crossorigin="anonymous"></script>
   <script src="<?= base_url('template/node_modules/bootstrap/dist/js/bootstrap.min.js') ?>"></script>
 
   <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
